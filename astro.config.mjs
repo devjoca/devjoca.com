f84873421f0213007cd7b26260aff8524e2e27de
joca <jpereyraleon@gmail.com>
2022-10-07 19:46:55 -0500

    chore: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,7 @@ import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-  // base: '.', // Set a path prefix.
   site: 'https://devjoca.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  // Important!
-  // Only official '@astrojs/*' integrations are currently supported by Astro.
-  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
-  // with 'astro build' command.
   experimental: {
     integrations: true,
   },
@@ -20,7 +15,7 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'monokai',
+      theme: 'nord',
     },
   },
   integrations: [
