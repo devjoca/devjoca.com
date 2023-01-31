d025b1989fc5f1ad6559a944f1fb9212159e599a
joca <jpereyraleon@gmail.com>
2023-01-30 23:34:11 -0500

    add content

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { SocialObjects } from "./types";
 
 export const SITE = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://devjoca.com",
+  author: "Joca Pereyra",
+  desc: "Welcome to Joca's tech blog! Join me, a seasoned backend developer, as I delve into the latest advancements in technology and AI/ML. I bring expertise in PHP, Python, Node.js, and TypeScript, and share my knowledge and insights through tutorials and tips. Stay ahead of the game and join me on this journey to continually improve and explore the world of software engineering.",
+  title: "devjoca",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -20,74 +20,26 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/devjoca",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
-    name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/josecarlos-pereyra",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:jpereyraleon@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     active: false,
   },
   {
     name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://twitter.com/devjoca",
     linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-  {
-    name: "Twitch",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitch`,
-    active: false,
-  },
-  {
-    name: "YouTube",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on YouTube`,
-    active: false,
-  },
-  {
-    name: "WhatsApp",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on WhatsApp`,
-    active: false,
-  },
-  {
-    name: "Snapchat",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Snapchat`,
-    active: false,
-  },
-  {
-    name: "Pinterest",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Pinterest`,
-    active: false,
-  },
-  {
-    name: "TikTok",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on TikTok`,
     active: false,
   },
   {
@@ -107,35 +59,5 @@ export const SOCIALS: SocialObjects = [
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on GitLab`,
     active: false,
-  },
-  {
-    name: "Reddit",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Reddit`,
-    active: false,
-  },
-  {
-    name: "Skype",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Skype`,
-    active: false,
-  },
-  {
-    name: "Steam",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Steam`,
-    active: false,
-  },
-  {
-    name: "Telegram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Mastodon`,
-    active: false,
-  },
+  }
 ];
---
+++ src/contents/my-first-post.md
@@ -0,0 +1,30 @@
+---
+author: Joca Pereyra
+datetime: 2023-01-12T15:22:00Z
+title: My first blog
+slug: my-first-blog
+featured: true
+tags: 
+    - post
+draft: false
+ogImage: ""
+description:
+  A short story about this blog
+---
+
+Since the last year I was trying to put some effort on a side project in order to learn some amazing new technologies or just for fun.
+There are a lot of complicated things about side projects, but for me was to actually start something so I choose building my personal blog to fill that purpose.
+
+## The Stack 🏗
+
+I like to consider me as a backend developer, however as most of us, sometimes we had to interact with some UI and one on the first technologies I learned after jQuery was VueJS. Time ago, with my experience on Vue, I pick Nuxt.js to develop a very simple landing website and it was really fun and a nice experience.
+
+For the moment, I want to try a new experience and [Astro](https://astro.build/) could really fulfill this purpose. It is going to be really cool to improve my React on TS knowledge.
+
+## Motivation 🔥
+
+I'm really passionate about sharing things and knowledge, usually, I share links with my friends or coworkers in our Slack and then save in [Pocket](https://getpocket.com/a/), but now I want to go one step further writing a little review as an entry post. This idea was heavily inspire in the [Freek Van der Herten’s blog](https://freek.dev/). I really admire this guy from the Laravel community, for example he suggests to blog about things we learned, so I'm taking this approach for now and beyond.
+
+https://twitter.com/freekmurze/status/1297202758618152960
+
+Lastly and not less important, improving my writing English was my other motivation, I hope this project is going to help me on that side. I’m very proud to finally lunch my blog. :)
