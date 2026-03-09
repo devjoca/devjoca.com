7826f8b8d4950149ec56fc76183d47f385d89d54
Joca <jpereyraleon@gmail.com>
2026-03-09 18:28:32 -0500

    better rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,6 +3,7 @@ import rss from "@astrojs/rss";
 import type { Frontmatter } from "src/types";
 import type { MarkdownInstance } from "astro";
 import slugify from "@utils/slugify";
+import sanitizeHtml from "sanitize-html";
 
 const postImportResult = import.meta.glob<MarkdownInstance<Frontmatter>>(
   "../contents/**/*.md",
@@ -19,10 +20,13 @@ export const GET = () =>
     site: SITE.website,
     items: posts
       .filter(({ frontmatter }) => !frontmatter.draft)
-      .map(({ frontmatter }) => ({
-        link: `posts/${slugify(frontmatter)}`,
-        title: frontmatter.title,
-        description: frontmatter.description,
-        pubDate: new Date(frontmatter.datetime),
+      .map(post => ({
+        link: `posts/${slugify(post.frontmatter)}`,
+        title: post.frontmatter.title,
+        description: post.frontmatter.description,
+        pubDate: new Date(post.frontmatter.datetime),
+        content: sanitizeHtml(post.compiledContent(), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
       })),
   });
